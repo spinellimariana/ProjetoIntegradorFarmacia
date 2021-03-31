d54a2801dac62bf2c30533521d62b7034769c248
spinellimariana <marispinelli@gmail.com>
2021-03-31 16:36:18 -0300

    esboço da tela de cadastro de cliente

--- ProjetoIntegradorFarmacia/src/main/java/com/mycompany/projetointegradorfarmacia/Main.java
+++ ProjetoIntegradorFarmacia/src/main/java/com/mycompany/projetointegradorfarmacia/Main.java
@@ -5,6 +5,12 @@
  */
 package com.mycompany.projetointegradorfarmacia;
 
+import com.sun.java.swing.plaf.windows.WindowsLookAndFeel;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+
 /**
  *
  * @author Lenovo
@@ -16,6 +22,13 @@ public class Main {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+
+        try {  //definindo o look and feel do windows
+           
+            UIManager.setLookAndFeel(new WindowsLookAndFeel());
+        } catch (UnsupportedLookAndFeelException ex) {
+            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
-    
+
 }
