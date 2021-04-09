bf2fd1ee27b00837454b84254d6d9a21e793526c
spinellimariana <marispinelli@gmail.com>
2021-04-09 18:09:27 -0300

    Mariana - 04.09.2021 Alterações: a) Classe Cliente; b) Classe Validadora; c) Tela de login; d) Tela Principal;

---
+++ ProjetoIntegradorFarmacia/src/main/java/com/mycompany/projetointegradorfarmacia/Cliente.java
@@ -0,0 +1,129 @@
+package com.mycompany.projetointegradorfarmacia;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+/**
+ *
+ * @author Lenovo
+ */
+public class Cliente {
+
+    private int CPF;
+    private int telCliente;
+    private int dataNascimento;
+    private int CEPcliente;
+    private String nomeCliente;
+    private String generoCliente;
+    private String estadoCivilCliente;
+    private String emailCliente;
+    private String enderecoCliente;
+    private String bairroCliente;
+    private String estadoCliente;
+    private String cidadeCliente;
+
+    public Cliente() { //construtor
+    }
+
+    //abaixo todos os getters and setters
+    public int getCPF() {
+        return CPF;
+    }
+
+    public void setCPF(int CPF) {
+        this.CPF = CPF;
+    }
+
+    public int getTelCliente() {
+        return telCliente;
+    }
+
+    public void setTelCliente(int telCliente) {
+        this.telCliente = telCliente;
+    }
+
+    public int getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public void setDataNascimento(int dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+
+    public int getCEPcliente() {
+        return CEPcliente;
+    }
+
+    public void setCEPcliente(int CEPcliente) {
+        this.CEPcliente = CEPcliente;
+    }
+
+    public String getNomeCliente() {
+        return nomeCliente;
+    }
+
+    public void setNomeCliente(String nomeCliente) {
+        this.nomeCliente = nomeCliente;
+    }
+
+    public String getGeneroCliente() {
+        return generoCliente;
+    }
+
+    public void setGeneroCliente(String generoCliente) {
+        this.generoCliente = generoCliente;
+    }
+
+    public String getEstadoCivilCliente() {
+        return estadoCivilCliente;
+    }
+
+    public void setEstadoCivilCliente(String estadoCivilCliente) {
+        this.estadoCivilCliente = estadoCivilCliente;
+    }
+
+    public String getEmailCliente() {
+        return emailCliente;
+    }
+
+    public void setEmailCliente(String emailCliente) {
+        this.emailCliente = emailCliente;
+    }
+
+    public String getEnderecoCliente() {
+        return enderecoCliente;
+    }
+
+    public void setEnderecoCliente(String enderecoCliente) {
+        this.enderecoCliente = enderecoCliente;
+    }
+
+    public String getBairroCliente() {
+        return bairroCliente;
+    }
+
+    public void setBairroCliente(String bairroCliente) {
+        this.bairroCliente = bairroCliente;
+    }
+
+    public String getEstadoCliente() {
+        return estadoCliente;
+    }
+
+    public void setEstadoCliente(String estadoCliente) {
+        this.estadoCliente = estadoCliente;
+    }
+
+    public String getCidadeCliente() {
+        return cidadeCliente;
+    }
+
+    public void setCidadeCliente(String cidadeCliente) {
+        this.cidadeCliente = cidadeCliente;
+    }
+
+}
