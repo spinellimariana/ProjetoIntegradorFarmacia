56fc078bc27137e8a615adb8d2a94212e4c4862b
vinicius-Sdr <vini.santana.612@gmail.com>
2021-04-11 23:44:02 -0300

    Adicionando tela e classe de vendas

---
+++ ProjetoIntegradorFarmacia/src/main/java/com/mycompany/projetointegradorfarmacia/Venda.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.projetointegradorfarmacia;
+
+/**
+ *
+ * @author vinic
+ */
+public class Venda {
+    
+    //possibilidade de tabela explosão para as vendas.
+    
+    private int idCliente;
+    private int idProduto;
+    private double valorFinal;
+    
+    public int getIdCliente() {
+        return idCliente;
+    }
+
+    public void setIdCliente(int idCliente) {
+        this.idCliente = idCliente;
+    }
+
+    public int getIdProduto() {
+        return idProduto;
+    }
+
+    public void setIdProduto(int idProduto) {
+        this.idProduto = idProduto;
+    }
+
+    public double getValorFinal() {
+        return valorFinal;
+    }
+
+    public void setValorFinal(double valorFinal) {
+        this.valorFinal = valorFinal;
+    }
+    
+}
