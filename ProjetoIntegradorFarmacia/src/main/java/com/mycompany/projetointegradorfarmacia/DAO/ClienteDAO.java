12f990431b14946c395b92692e2f3c5f613f2550
spinellimariana <marispinelli@gmail.com>
2021-05-06 15:35:16 -0300

    botão salvar (ok), iniciei listagem total de clientes

--- ProjetoIntegradorFarmacia/src/main/java/com/mycompany/projetointegradorfarmacia/DAO/ClienteDAO.java
+++ ProjetoIntegradorFarmacia/src/main/java/com/mycompany/projetointegradorfarmacia/DAO/ClienteDAO.java
@@ -5,10 +5,128 @@
  */
 package com.mycompany.projetointegradorfarmacia.DAO;
 
+import com.mycompany.projetointegradorfarmacia.model.Cliente;
+import com.mycompany.projetointegradorfarmacia.utils.GerenciadorConexao;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+
 /**
  *
  * @author Lenovo
  */
 public class ClienteDAO {
-    
+
+    public static boolean salvar(Cliente p) {
+        boolean retorno = false;
+        Connection conexao = null;
+        PreparedStatement instrucaoSQL = null;
+
+        try {
+            conexao = GerenciadorConexao.abrirConexao();
+
+            instrucaoSQL = conexao.prepareStatement("INSERT INTO cliente (CPF, nome, genero, estadoCivil, telCliente, "
+                    + "dtNascimento, email, endereco, bairro, cidade, estado, CEP) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, "
+                    + "?, ?)", Statement.RETURN_GENERATED_KEYS);
+
+            //parametros para gravar
+            instrucaoSQL.setString(1, p.getCPFSomenteNumeros());
+            instrucaoSQL.setString(2, p.getNomeCliente());
+            instrucaoSQL.setString(3, p.getGeneroCliente());
+            instrucaoSQL.setString(4, p.getEstadoCivilCliente());
+            instrucaoSQL.setString(5, p.getTelSomenteNumeros());  //ver se dá pra fazer com a formatação
+            instrucaoSQL.setString(6, p.getDataSomenteNumeros()); //refazer usando aula 10 jCalendar //ver se dá pra fazer com a formatação
+            instrucaoSQL.setString(7, p.getEmailCliente());
+            instrucaoSQL.setString(8, p.getEnderecoCliente());
+            instrucaoSQL.setString(9, p.getBairroCliente());
+            instrucaoSQL.setString(10, p.getCidadeCliente());
+            instrucaoSQL.setString(11, p.getEstadoCliente());
+            instrucaoSQL.setString(12, p.getCEPcliente());  //ver se dá pra fazer com a formatação
+
+            int linhasAfetadas = instrucaoSQL.executeUpdate();
+
+            if (linhasAfetadas > 0) {
+                retorno = true;
+
+                ResultSet generatedKeys = instrucaoSQL.getGeneratedKeys(); //para recuperar o id do cliente
+                if (generatedKeys.next()) {
+                    p.setId(generatedKeys.getInt(1));
+                } else {
+                    throw new SQLException("Falha ao obter ID do cliente!");
+                }
+            } else {
+                retorno = false;
+            }
+
+        } catch (SQLException | ClassNotFoundException ex) {
+            System.out.println(ex.getMessage());
+            retorno = false;
+        } finally {
+            //liberar recursos da memória
+            try {
+                if (instrucaoSQL != null) {
+                    instrucaoSQL.close();
+                }
+
+                GerenciadorConexao.fecharConexao();
+            } catch (SQLException ex) {
+            }
+        }
+        return retorno;
+    }
+
+    public static ArrayList<Cliente> listarClientes() {
+        ResultSet rs = null;
+        Connection conexao = null;
+        PreparedStatement instrucaoSQL = null;
+
+        ArrayList<Cliente> listar = new ArrayList<Cliente>();
+
+        try {
+            conexao = GerenciadorConexao.abrirConexao();
+            instrucaoSQL = conexao.prepareStatement("SELECT * FROM cliente;");
+            rs = instrucaoSQL.executeQuery(); //executar a query
+
+            //percorrer o result set
+            while (rs.next()) {
+                Cliente c = new Cliente();
+                c.setId(rs.getInt("id"));
+                c.setCPF(rs.getString("CPF"));
+                c.setNomeCliente(rs.getString("nome"));
+                c.setGeneroCliente(rs.getString("genero"));
+                c.setEstadoCivilCliente(rs.getString("estadoCivil"));
+                c.setTelCliente(rs.getString("telCliente"));
+                c.setDataNascimento(rs.getString("dtNascimento")); //refazer usando aula 10 jCalendar
+                c.setEmailCliente(rs.getString("email"));
+                c.setEnderecoCliente(rs.getString("endereco"));
+                c.setBairroCliente(rs.getString("bairro"));
+                c.setCidadeCliente(rs.getString("cidade"));
+                c.setEstadoCliente(rs.getString("estado"));
+                c.setCEPcliente(rs.getString("CEP"));
+
+                listar.add(c);
+
+            }
+
+        } catch (SQLException | ClassNotFoundException ex) {
+            System.out.println(ex.getMessage());
+            listar = null;
+        } finally {
+            try {
+                if (rs != null) {
+                    rs.close();
+                }
+                if (instrucaoSQL != null) {
+                    instrucaoSQL.close();
+                }
+                GerenciadorConexao.fecharConexao();
+                
+            } catch (SQLException ex) {
+            }
+        }
+        return listar;
+    }
 }
--- ProjetoIntegradorFarmacia/src/main/java/com/mycompany/projetointegradorfarmacia/controller/ClienteController.java
+++ ProjetoIntegradorFarmacia/src/main/java/com/mycompany/projetointegradorfarmacia/controller/ClienteController.java
@@ -5,10 +5,48 @@
  */
 package com.mycompany.projetointegradorfarmacia.controller;
 
+import com.mycompany.projetointegradorfarmacia.model.Cliente;
+import com.mycompany.projetointegradorfarmacia.DAO.ClienteDAO;
+import java.util.ArrayList;
+
 /**
  *
  * @author Lenovo
  */
 public class ClienteController {
-    
+
+    public static boolean salvar(String CPF, String nome, String genero, String estadoCivil, String telefone,
+            String dtNascimento, String email, String endereco, String bairro, String cidade, String estado, String CEP) {
+
+        Cliente objSalvar = new Cliente();
+
+        objSalvar.setCPF(CPF);
+        objSalvar.setNomeCliente(nome);
+        objSalvar.setGeneroCliente(genero);
+        objSalvar.setEstadoCivilCliente(estadoCivil);
+        objSalvar.setTelCliente(telefone);
+        objSalvar.setDataNascimento(dtNascimento);  //usar jCalendar
+        objSalvar.setEmailCliente(email);
+        objSalvar.setEnderecoCliente(endereco);
+        objSalvar.setBairroCliente(bairro);
+        objSalvar.setCidadeCliente(cidade);
+        objSalvar.setEstadoCliente(estado);
+        objSalvar.setCEPcliente(CEP);
+
+        return ClienteDAO.salvar(objSalvar);
+    }
+
+    public static ArrayList<String[]> listarC() {
+        ArrayList<Cliente> listaClientes = ClienteDAO.listarClientes();
+        ArrayList<String[]> retorno = new ArrayList<>();
+
+        for (Cliente item : listaClientes) {
+            retorno.add(new String[]{String.valueOf(item.getId()), item.getNomeCliente(), item.getGeneroCliente(),
+                item.getEstadoCivilCliente(), item.getTelCliente(), item.getDataNascimento(), item.getEmailCliente(),
+                item.getEnderecoCliente(), item.getBairroCliente(), item.getCidadeCliente(), item.getEstadoCliente(),
+                item.getCEPcliente()});
+
+        }
+        return retorno;
+    }
 }
